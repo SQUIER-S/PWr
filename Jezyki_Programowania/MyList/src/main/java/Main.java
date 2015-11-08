2856c286f816371f964781fb28b94b3fc0576726
Squier <rafal.sz.49@gmail.com>
2015-11-08 20:49:22 +0100

    Finished exercise number 1

--- Jezyki_Programowania/MyList/src/main/java/Main.java
+++ Jezyki_Programowania/MyList/src/main/java/Main.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by SQUIER
  * on 2015-11-06.
@@ -9,34 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<String> a = new ArrayList<>();
 
-        MyArrayList<String> b = new MyArrayList<>();
-
-        b.add("Hello");
-        b.add("Hello");
-        b.add("Hello");
-        b.add("Hello");
-        b.add("Hello");
-        b.add("Hello");
-        b.add("Hello");
-        b.add("World");
-
-        a.add("this");
-        a.add("sucks");
-
-        b.addAll(a);
-
-        for (int i = 0; i < b.size(); i++) {
-
-            System.out.println(b.get(i) + " " + b.size());
-        }
-
-        System.out.println(b.contains("Hello"));
-        System.out.println(b.contains("HelloW"));
-        System.out.println(b.indexOf("this"));
-        System.out.println(b.indexOf("HelloW"));
-        System.out.println(b.lastIndexOf("Hello"));
     }
 
 }
--- Jezyki_Programowania/MyList/src/main/java/MyArrayList.java
+++ Jezyki_Programowania/MyList/src/main/java/MyArrayList.java
@@ -7,59 +7,108 @@ import java.util.Collection;
  */
 public class MyArrayList<E> {
 
+    /**
+     * Default capacity
+     */
     private static final int DEFAULT_CAPACITY = 10;
-    private Object array[];
-    private int index;
 
+    /**
+     * Array that is used to store objects
+     */
+    private Object array[];
+
+    /**
+     * Actual size of list
+     */
+    private int size;
+
+    /**
+     * Default constructor.
+     * Creates new array with initial DEFAULT_CAPACITY.
+     * Sets size to zero
+     */
     public MyArrayList() {
 
         array = new Object[DEFAULT_CAPACITY];
-        index = 0;
+        size = 0;
     }
 
+    /**
+     * @return  - size of an array list
+     */
     public int size() {
-        return index;
+        return size;
     }
 
-    public boolean isEmpty() {return index == 0;}
+    /**
+     * Checks if array is empty or not
+     * @return  - true if size equals zero, false if size is greater than zero
+     */
+    public boolean isEmpty() {return size == 0;}
 
+    /**
+     * Adds element at the end of the array list.
+     * Enlarges capacity if needed
+     * @param element   - element to be added
+     */
     public void add(E element) {
 
-        if(array.length == index) {
+        if(array.length == size) {
             ensureCapacity(DEFAULT_CAPACITY);
         }
 
-        array[index++] = element;
+        array[size++] = element;
 
     }
 
+    /**
+     * Merges array with collection.
+     * Adds collection at the end of array
+     * @param collection    - collection of elements to be added
+     */
     public void addAll(Collection<? extends E> collection) {
         Object[] a = collection.toArray();
         int sizeToAdd = a.length;
         ensureCapacity(sizeToAdd);
-        System.arraycopy(a, 0, array, index, sizeToAdd);
-        index += sizeToAdd;
+        System.arraycopy(a, 0, array, size, sizeToAdd);
+        size += sizeToAdd;
     }
 
     @SuppressWarnings("unchecked")
+    /**
+     * Get the index'th element.
+     * Throws ArrayIndexOutOfBoundsException
+     * if index is out of array range or is negative
+     * @param index - index of element to get
+     * @return index'th element
+     */
     public E get(int index) {
-        if(index >= this.index) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + this.index);
+        if(index >= this.size) {
+            throw new ArrayIndexOutOfBoundsException("Index: " + index + ", Size: " + this.size);
         } else {
             return (E)array[index];
         }
     }
 
+    /**
+     * Checks if array contains certain object
+     * @param object    - object to check if is stored
+     * @return  true if object exists in array, false if array doesn't store such object
+     */
     public boolean contains(Object object) {return indexOf(object) >= 0;}
 
+    /**
+     * @return  - index of firs occurrence of specified element,
+     * -1 if elements doesn't exist in array
+     */
     public int indexOf(Object object) {
 
         if(object == null) {
-            for (int i = 0; i < index; i++)
+            for (int i = 0; i < size; i++)
                 if (array[i]==null)
                     return i;
         } else {
-            for (int i = 0; i < index; i++)
+            for (int i = 0; i < size; i++)
                 if (object.equals(array[i]))
                     return i;
         }
@@ -67,14 +116,17 @@ public class MyArrayList<E> {
 
     }
 
+    /**
+     * @return - index of last occurrence of the specified element
+     */
     public int lastIndexOf(Object object) {
 
         if(object == null) {
-            for (int i = index - 1; i >= 0; i--)
+            for (int i = size - 1; i >= 0; i--)
                 if (array[i]==null)
                     return i;
         } else {
-            for (int i = index - 1; i >= 0; i--)
+            for (int i = size - 1; i >= 0; i--)
                 if (object.equals(array[i]))
                     return i;
         }
@@ -82,8 +134,13 @@ public class MyArrayList<E> {
 
     }
 
+    /**
+     * Sets certain value at the specified position of array
+     * @throws ArrayIndexOutOfBoundsException
+     * @return - previous element on this position
+     */
     public E set(int index, E newValue) {
-        if(this.index <= index) throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + this.index);
+        if(this.size <= index) throw new ArrayIndexOutOfBoundsException("Index: " + index + ", Size: " + this.size);
         else {
             E oldValue = (E)array[index];
             array[index] = newValue;
@@ -91,8 +148,15 @@ public class MyArrayList<E> {
         }
     }
 
-    public Object[] toArray() {return Arrays.copyOf(array, index);}
+    /**
+     * Transforms array list to common array
+     * @return  - common array
+     */
+    public Object[] toArray() {return Arrays.copyOf(array, size);}
 
+    /*
+     * Enlarges capacity by given parameter
+     */
     private void ensureCapacity(int sizeToAdd) {
         array = Arrays.copyOf(array, array.length + sizeToAdd);
     }
